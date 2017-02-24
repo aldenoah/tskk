a249b634be9760a9a940e303e5b932d80e7cd218
Phidias Lo <phidiaslo@gmail.com>
2017-02-24 21:38:25 +0800

    add robots

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def disable_json
+    if request.format.to_s =~ /json/ 
+      redirect_to root_url, notice: "We're sorry we couldn't find the page you were looking for!"
+    end
+  end
+
+  helper_method :mobile_device?
+  private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
+  end
 end
