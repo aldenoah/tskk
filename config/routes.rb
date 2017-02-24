0b8a913d381bc3ea4f9f423cda0c4b1c20c23b43
Phidias Lo <phidiaslo@gmail.com>
2017-02-24 23:59:26 +0800

    add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "contact" => "contacts#new"
+
+  resources :contacts
   get '*unmatched_route', to: 'application#raise_not_found'
   #get "post" => 'posts#index'
   get "sitemap.xml" => "sitemap#index", :format => "xml", :as => :sitemap
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221071058) do
+ActiveRecord::Schema.define(version: 20170224152712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.text     "message"
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
