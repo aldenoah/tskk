912fc84709d36c919d8f181f4f1f8105fb7400a3
Phidias Lo <phidiaslo@gmail.com>
2017-02-24 21:26:45 +0800

    style devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   #get "post" => 'posts#index'
   get "sitemap.xml" => "sitemap#index", :format => "xml", :as => :sitemap
 
-  resources :posts, path: "kota-kinabalu"
+  resources :posts
   devise_for :users
   
   root 'welcome#index'
